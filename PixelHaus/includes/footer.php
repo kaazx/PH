81895a16dddaffd4df76d46f3f29d2b2b37dab5a
Kirsten Zapanta <keithleenzapanta@gmail.com>
2026-03-16 15:49:37 +0800

    Delete PixelHaus directory

--- PixelHaus/includes/footer.php
+++
@@ -1,9 +0,0 @@
-<html lang="en">
-    <footer>
-      <div class="container">
-        &copy; PixelHaus <?= date('Y'); ?>
-      </div>
-    </footer>
-    <script src="js/site.js"></script>
-  </body>
-</html>
